2b6552410d3f7d4a5486a291d1033d0d90bffc0e
lappcihan <aum.amen@gmail.com>
2020-05-31 20:46:22 +0900

    geanyでdark-themeをまとめてダウンロード(linux用)

---
+++ geany-themedl.sh
@@ -0,0 +1,29 @@
+#!/bin/sh
+
+##### geany-themedl.sh
+##### 2020-05-31 lappichan writtened
+
+cd  ~/.config/geany/colorschemes/
+wget -q https://raw.github.com/geany/geany-themes/master/colorschemes/bespin.conf
+wget -q https://raw.github.com/geany/geany-themes/master/colorschemes/black.conf
+wget -q https://raw.github.com/geany/geany-themes/master/colorschemes/darcula.conf
+wget -q https://raw.github.com/geany/geany-themes/master/colorschemes/dark.conf
+wget -q https://raw.github.com/geany/geany-themes/master/colorschemes/dark-colors.conf
+wget -q https://raw.github.com/geany/geany-themes/master/colorschemes/dark-fruit-salad.conf
+wget -q https://raw.github.com/geany/geany-themes/master/colorschemes/delt-dark.conf
+wget -q https://raw.github.com/geany/geany-themes/master/colorschemes/himbeere.conf
+wget -q https://raw.github.com/geany/geany-themes/master/colorschemes/inkpot.conf
+wget -q https://raw.github.com/geany/geany-themes/master/colorschemes/monokai.conf
+wget -q https://raw.github.com/geany/geany-themes/master/colorschemes/oblivion2.conf
+wget -q https://raw.github.com/geany/geany-themes/master/colorschemes/railcasts2.conf
+wget -q https://raw.github.com/geany/geany-themes/master/colorschemes/mc.conf
+wget -q https://raw.github.com/geany/geany-themes/master/colorschemes/retro.conf
+wget -q https://raw.github.com/geany/geany-themes/master/colorschemes/sleepy-pastel.conf
+wget -q https://raw.github.com/geany/geany-themes/master/colorschemes/spyder-dark.conf
+wget -q https://raw.github.com/geany/geany-themes/master/colorschemes/solarized-dark.conf
+wget -q https://raw.github.com/geany/geany-themes/master/colorschemes/tango-dark.conf
+wget -q https://raw.github.com/geany/geany-themes/master/colorschemes/vibrant-ink.conf
+wget -q https://raw.github.com/geany/geany-themes/master/colorschemes/steampunk.conf
+wget -q https://raw.github.com/geany/geany-themes/master/colorschemes/tinge.conf
+wget -q https://raw.github.com/geany/geany-themes/master/colorschemes/zenburn.conf
+exit 0
